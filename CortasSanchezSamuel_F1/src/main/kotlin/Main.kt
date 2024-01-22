913fe800f4369276d1b11d1e7351daf34989d545
Samuel Cortés Sánchez <132077654+Samuceese@users.noreply.github.com>
2024-01-22 13:09:15 +0100

    Cambio del main

--- CortasSanchezSamuel_F1/src/main/kotlin/Main.kt
+++ CortasSanchezSamuel_F1/src/main/kotlin/Main.kt
@@ -4,7 +4,7 @@ fun main(){
     println("-----------🏎️✨SEAN BIENVENIDOS AL PRIMER GRAN PREMIO DE DAW✨🏎️-----------")
     println("Cuatro equipos de Fórmula 1 competirán en esta emocionante carrera")
     println("¡Prepárense para una experiencia única en el mundo automovilístico!")
-    println(".-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.PARRILLA DE SALIDA.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.")
+    println(".-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.🏁PARRILLA DE SALIDA🏁.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.")
 
     val circuito=Circuito()
     circuito.darSalida()
